ea679f10426a72b9be716c6ee2f476dc6ed18a63
garyparrot <weedbug666@gmail.com>
2019-07-26 17:49:21 +0800

    Extract functions as a seprarte source file

--- chapter20/show_sigset.c
+++ chapter20/show_sigset.c
@@ -39,48 +39,7 @@
 #include <signal.h>
 
 #include "tlpi_hdr.h"
-
-void printSigset(FILE *of, const char *prefix, const sigset_t *sigset){
-    int cnt = 0;
-
-    for(int sig = 1; sig < NSIG; sig++){
-        if(sigismember(sigset, sig)){
-            cnt++;
-            fprintf(of, "%s%d (%s)\n", prefix, sig, strsignal(sig));
-        }
-    }
-
-    if(cnt == 0)
-        fprintf(of, "%s<empty signal set>\n", prefix);
-}
-
-/* Show blocked signal for this process */
-int printSigMask(FILE *of, const char *msg){
-    sigset_t currMask;
-
-    if(msg != NULL) fprintf(of, "%s", msg);
-
-    if(sigprocmask(SIG_BLOCK, NULL, &currMask) == -1)
-        return -1;
-
-    printSigset(of, "\t\t", &currMask);
-
-    return 0;
-}
-
-/* Show pending signal */
-int printPendingSigs(FILE *of, const char *msg){
-    sigset_t pendingSigs;
-
-    if(msg != NULL) fprintf(of, "%s", msg);
-
-    if(sigpending(&pendingSigs) == -1)
-        return -1;
-
-    printSigset(of, "\t\t", &pendingSigs);
-
-    return 0;
-}
+#include "signal_functions.h"
 
 int main(int argc, const char *argv[]){
     
--- chapter20/sig_receiver.c
+++ chapter20/sig_receiver.c
@@ -22,47 +22,7 @@
 
 #include "tlpi_hdr.h"
 
-void printSigset(FILE *of, const char *prefix, const sigset_t *sigset){
-    int cnt = 0;
-
-    for(int sig = 1; sig < NSIG; sig++){
-        if(sigismember(sigset, sig)){
-            cnt++;
-            fprintf(of, "%s%d (%s)\n", prefix, sig, strsignal(sig));
-        }
-    }
-
-    if(cnt == 0)
-        fprintf(of, "%s<empty signal set>\n", prefix);
-}
-
-/* Show blocked signal for this process */
-int printSigMask(FILE *of, const char *msg){
-    sigset_t currMask;
-
-    if(msg != NULL) fprintf(of, "%s", msg);
-
-    if(sigprocmask(SIG_BLOCK, NULL, &currMask) == -1)
-        return -1;
-
-    printSigset(of, "\t\t", &currMask);
-
-    return 0;
-}
-
-/* Show pending signal */
-int printPendingSigs(FILE *of, const char *msg){
-    sigset_t pendingSigs;
-
-    if(msg != NULL) fprintf(of, "%s", msg);
-
-    if(sigpending(&pendingSigs) == -1)
-        return -1;
-
-    printSigset(of, "\t\t", &pendingSigs);
-
-    return 0;
-}
+#include "signal_function.h"
 
 static int sigCnt[NSIG];
 static volatile sig_atomic_t gotSigint= 0;
